a5951e03de065647d307105c37b98336d8349968
Otoglovi <darlington.2004@gmail.com>
2017-04-14 02:50:28 +0100

    Initial commit

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 14-Apr-17
+ * Time: 01:33 AM
+ */
+include (dbConnect.php); //Establishing connection with database
+
+//check if the username and password have values
+if (empty($_POST["username"]) || empty($_POST["password"]))
+{
+    echo "Both fields are required";
+}else {
+    //assign local variables to the parameters passed in via the POST
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+//create an SQL statement ( a query) that selects the uid of a
+// user who has the username and password entered
+    $sql = "SELECT uid FROM users WHERE username = '$username' AND
+password = '$password'";
+
+//set up a variable, "result", to hold the result of the query
+    $result = mysqli_query($db, $sql);
+
+//check how big the result is; i.e. no viable user found or more than
+//one user found
+    if (mysqli_num_rows($result) == 1) {
+        header("location: home.php"); //Redirect to another page
+    } else {
+        echo "Incorrect username or password";
+    }
+}
+
+?>
